f49a7254f54fd168ccdc147961ec857431d24237
VinceVT <vincevutruong@gmail.com>
2019-04-02 16:24:12 -0700

    dependencies updated

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import './assets/stylesheets/styles.scss';
 import React from 'react'; // import the main react dependency
 import ReactDOM from 'react-dom'; // import reactDOM
 import App from './app/App.jsx'; // import the main app component
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root')); // render our App component and mount it to our #root element
+ReactDOM.render(<Provider>  
+                <App />
+                </Provider>, 
+                document.getElementById('root')); // render our App component and mount it to our #root element
